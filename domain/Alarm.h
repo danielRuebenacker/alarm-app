e1c4f47eb549b8cef4f90d71b898805dbf2237ba
danielRuebenacker <danielrubenacker2@gmail.com>
2026-05-29 17:10:52 +0100

    Update alarm data class: add shouldFire method which takes a time and returns whether alarm should be going now

--- domain/Alarm.h
+++ domain/Alarm.h
@@ -1,7 +1,23 @@
-struct Alarm {
+#include <ctime>
+
+enum PuzzleType {
+	MATHS,
+	MEMORY,
+	CAPTCHA
+};
+
+struct AlarmData {
 	int hour;
 	int min;
 	bool isActive;
-	int snooze_time;
-	int max_snoozes;
+	int snoozeTimeMinutes;
+	int maxSnoozes;
+	PuzzleType puzzleType;
+
+	bool shouldFire(const std::tm& localTime) const {
+		if (!isActive) {
+			return false;
+		}
+		return (localTime.tm_hour == hour && localTime.tm_min == min);
+	}
 };
