cbc128211bd1a55eaed195b50ca9d7264bd4d48a
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:11:24 +0100

    Change back to using std style and add method exposing the clock

--- domain/AlarmContext.h
+++ domain/AlarmContext.h
@@ -3,10 +3,6 @@
 #include <vector>
 #include "../interfaces/IAppState.h"
 
-using std::unique_ptr;
-using std::shared_ptr;
-using std::vector;
-
 class ISound;
 class IClock;
 class IHomeScreen;
@@ -16,17 +12,18 @@ struct AlarmData;
 // the app's state machine
 class AlarmContext {
 	private:
-		unique_ptr<IAppState> currentState;
-		shared_ptr<ISound> soundSys;
-		shared_ptr<IClock> clockSys;
-		shared_ptr<IHomeScreen> homeScreen;
-		shared_ptr<IRingingScreen> ringingScreen;
-		vector<AlarmData> alarms;
+		std::unique_ptr<IAppState> currentState;
+		std::shared_ptr<ISound> soundSys;
+		std::shared_ptr<IClock> clockSys;
+		std::shared_ptr<IHomeScreen> homeScreen;
+		std::shared_ptr<IRingingScreen> ringingScreen;
+		std::vector<AlarmData> alarms;
 	public:
-          AlarmContext(shared_ptr<ISound> sound, shared_ptr<IClock> clock,
-                       shared_ptr<IHomeScreen> home,
-                       shared_ptr<IRingingScreen> ringing);
+          AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock,
+                       std::shared_ptr<IHomeScreen> home,
+                       std::shared_ptr<IRingingScreen> ringing);
 
-		  void changeState(unique_ptr<IAppState> newState);
+		  void changeState(std::unique_ptr<IAppState> newState);
 		  void update();
+	std::shared_ptr<IClock> getClock();
 };
