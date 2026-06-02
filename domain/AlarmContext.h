d59695044c46761075d6783d58d5c285c1136031
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 14:24:38 +0100

    Add current screen instance variable to context

--- domain/AlarmContext.h
+++ domain/AlarmContext.h
@@ -2,11 +2,13 @@
 #include <memory>
 #include <vector>
 #include "./Alarm.h"
+#include "./ScreenRepository.h"
 
 class ISound;
 class IClock;
 class IInput;
 class IAppState;
+class IScreen;
 
 // the app's state machine
 class AlarmContext {
@@ -16,6 +18,8 @@ class AlarmContext {
 		std::shared_ptr<IClock> clockSys_;
 		std::shared_ptr<IInput> input_;
 		std::vector<Alarm> alarms_;
+		std::shared_ptr<ScreenRepository> screenRepo_;
+		std::shared_ptr<IScreen> currentScreen_;
 	public:
 		AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock, std::shared_ptr<IInput> input);
 
