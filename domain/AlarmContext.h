11e8669285d2a962fdd77c744b0414d5043501a6
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:53:25 +0100

    Add method to expose sound

--- domain/AlarmContext.h
+++ domain/AlarmContext.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <memory>
 #include <vector>
+#include "./Alarm.h"
 
 class ISound;
 class IClock;
 class IAppState;
-struct AlarmData;
 
 // the app's state machine
 class AlarmContext {
@@ -13,11 +13,12 @@ class AlarmContext {
 		std::unique_ptr<IAppState> currentState;
 		std::shared_ptr<ISound> soundSys;
 		std::shared_ptr<IClock> clockSys;
-		std::vector<AlarmData> alarms;
+		std::vector<Alarm> alarms;
 	public:
-          AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock);
+		AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock);
 
-		  void changeState(std::unique_ptr<IAppState> newState);
-		  void update();
-	std::shared_ptr<IClock> getClock();
+		void changeState(std::unique_ptr<IAppState> newState);
+		void update();
+		std::shared_ptr<IClock> getClock();
+		std::shared_ptr<ISound> getSound();
 };
