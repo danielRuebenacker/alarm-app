f4f0d6feb0b4f36a2df7e4c26eba9fe340ca2504
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:37:14 +0100

    Promote time point to a class and add minutesSinceMidnight method

--- types/TimePoint.h
+++ types/TimePoint.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <stdexcept>
 
-struct TimePoint {
-	int hour;
-	int min;
+class TimePoint {
+	public:
+		TimePoint(int hour, int min) {
+			if (hour < 0 || hour > 23 ||
+            min < 0 || min > 59)
+            throw std::invalid_argument("invalid time");
+
+			hour_ = hour;
+			min_ = min;
+		}
+
+		int minutesSinceMidnight() {
+			return hour_ * 60 + min_;
+		};
+
+		int hour() const { return hour_; }
+		int min() const { return min_; }
+
+	private:
+		int hour_;
+		int min_;
 };
