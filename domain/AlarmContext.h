e048f3c4cdbe5cb3070524021fdbf3fa5e617b49
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:49:09 +0100

    No longer using separate screens, will use screen manager.

--- domain/AlarmContext.h
+++ domain/AlarmContext.h
@@ -1,12 +1,10 @@
 #pragma once
 #include <memory>
 #include <vector>
-#include "../interfaces/IAppState.h"
 
 class ISound;
 class IClock;
-class IHomeScreen;
-class IRingingScreen;
+class IAppState;
 struct AlarmData;
 
 // the app's state machine
@@ -15,13 +13,9 @@ class AlarmContext {
 		std::unique_ptr<IAppState> currentState;
 		std::shared_ptr<ISound> soundSys;
 		std::shared_ptr<IClock> clockSys;
-		std::shared_ptr<IHomeScreen> homeScreen;
-		std::shared_ptr<IRingingScreen> ringingScreen;
 		std::vector<AlarmData> alarms;
 	public:
-          AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock,
-                       std::shared_ptr<IHomeScreen> home,
-                       std::shared_ptr<IRingingScreen> ringing);
+          AlarmContext(std::shared_ptr<ISound> sound, std::shared_ptr<IClock> clock);
 
 		  void changeState(std::unique_ptr<IAppState> newState);
 		  void update();
