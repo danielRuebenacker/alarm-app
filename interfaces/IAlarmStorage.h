225a6c8219e5a1d154117492165fd87b20a8e5de
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:12:23 +0100

    Changed name from AlarmData to Alarm for Alarm class

--- interfaces/IAlarmStorage.h
+++ interfaces/IAlarmStorage.h
@@ -1,11 +1,10 @@
 #include <vector>
 #include "../domain/Alarm.h"
-using std::vector;
 
 // allows to save to/load from flash memory/SD card
 class IAlarmStorage {
 	public: 
 		virtual ~IAlarmStorage() = default;
-		virtual void saveAlarms(const vector<AlarmData> alarms) = 0;
-		virtual vector<AlarmData> loadAlarms() = 0;
+		virtual void saveAlarms(const std::vector<Alarm> alarms) = 0;
+		virtual std::vector<Alarm> loadAlarms() = 0;
 };
