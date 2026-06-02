50afc45f658ad0084ce70bcd86375d55e62b2554
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:08:16 +0100

    Add DayMask to alarm and turn into a class instead of struct

--- domain/Alarm.h
+++ domain/Alarm.h
@@ -1,4 +1,6 @@
-#include <ctime>
+#include <cstdint>
+#include "../structs/TimePoint.h"
+#include "../interfaces/IClock.h"
 
 enum PuzzleType {
 	MATHS,
@@ -6,18 +8,33 @@ enum PuzzleType {
 	CAPTCHA
 };
 
-struct AlarmData {
-	int hour;
-	int min;
-	bool isActive;
-	int snoozeTimeMinutes;
-	int maxSnoozes;
-	PuzzleType puzzleType;
+enum DayMask : uint8_t
+{
+    Sunday    = 1 << 0, // 00000001
+    Monday    = 1 << 1, // 00000010
+    Tuesday   = 1 << 2, // 00000100
+    Wednesday = 1 << 3, // 00001000
+    Thursday  = 1 << 4, // 00010000
+    Friday    = 1 << 5, // 00100000
+    Saturday  = 1 << 6  // 01000000
+};
 
-	bool shouldFire(const std::tm& localTime) const {
-		if (!isActive) {
-			return false;
-		}
-		return (localTime.tm_hour == hour && localTime.tm_min == min);
-	}
+class Alarm {
+	private:
+		TimePoint time;
+		bool isActive;
+		bool hasTriggered;
+		int snoozeMinutes;
+		int maxSnoozes;
+		PuzzleType puzzleType;
+		uint8_t dayMask;
+
+	public:
+		Alarm(TimePoint t, PuzzleType puzzle, uint8_t days);
+		TimePoint getTime();
+		int getMinutesUntilRing();
+		bool snooze();
+		bool turnOff();
+		bool turnOn();
+		bool shouldTrigger(const TimePoint currentTime);
 };
