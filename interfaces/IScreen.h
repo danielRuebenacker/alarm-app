9bc720f992d375c4c692324893c3e766d1b52e71
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-02 10:13:22 +0100

    Add exit method for cleaning up resources for screen

--- interfaces/IScreen.h
+++ interfaces/IScreen.h
@@ -3,4 +3,5 @@ class IScreen {
 		virtual ~IScreen() = default;
 		virtual void load() = 0;	// when screen initialised
 		virtual void update() = 0;	// called periodically
+		virtual void exit() = 0;	// cleaning up
 };
