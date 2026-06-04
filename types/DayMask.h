f329c59b9dfb7ec833222ff31c69634ed276a668
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-04 11:49:06 +0100

    Make DayMask an enum class for type safety and provide methods for pipe and and operator

--- types/DayMask.h
+++ types/DayMask.h
@@ -1,6 +1,6 @@
 #include <cstdint>
 
-enum DayMask : uint8_t {
+enum class DayMask : uint8_t {
     Sunday    = 1 << 0, // 00000001
     Monday    = 1 << 1, // 00000010
     Tuesday   = 1 << 2, // 00000100
@@ -9,3 +9,11 @@ enum DayMask : uint8_t {
     Friday    = 1 << 5, // 00100000
     Saturday  = 1 << 6  // 01000000
 };
+
+inline DayMask operator|(DayMask lhs, DayMask rhs) {
+	return static_cast<DayMask>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs));
+}
+
+inline bool operator&(DayMask lhs, DayMask rhs) {
+	return static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs);
+}
