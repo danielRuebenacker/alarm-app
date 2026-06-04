44f78e96646253c792aa09268775e67c2c4a32cf
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-04 11:48:33 +0100

    Add method declaration to Clock interface for getting current day (DayMask)

--- interfaces/IClock.h
+++ interfaces/IClock.h
@@ -1,9 +1,11 @@
 #include <ctime>
 #include "../types/TimePoint.h"
+#include "../types/DayMask.h"
 
 // interface to supply the current time
 class IClock {
 	public:
 		virtual ~IClock() = default;
 		virtual TimePoint now() const = 0;
+		virtual DayMask getCurrentDay() const = 0;
 };
