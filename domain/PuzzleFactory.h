29b6d6ef96e441878a04ffbc8ffe339671d265ea
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-06 21:56:19 +0100

    Move PuzzleFactory.cpp to .h

---
+++ domain/PuzzleFactory.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../interfaces/IPuzzle.h"
+#include "../types/PuzzleType.h"
+#include "../puzzles/MathPuzzle.cpp"
+
+#include <memory>
+#include <stdexcept>
+
+class PuzzleFactory {
+	public:
+		static std::unique_ptr<IPuzzle> createPuzzle(PuzzleType type) {
+			switch(type) {
+				case PuzzleType::MATHS:
+					return std::make_unique<MathPuzzle>();
+				case PuzzleType::CAPTCHA:
+					return nullptr;
+				default:
+					throw std::invalid_argument("Unknown puzzle type requested."); 
+			} 
+		}
+};
