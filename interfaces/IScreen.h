af91bd2dba1bb801f68af947018fb3968b10bf6d
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-07 10:20:31 +0100

    Add getContentContainer method to IScreen
    Returns a content container UI Widget if the screen allows nesting (e.g.
    puzzle screens return a section to be populated with the actual puzzle)

--- interfaces/IScreen.h
+++ interfaces/IScreen.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "IUIWidget.h"
 
 class IScreen {
 	public:
@@ -6,4 +7,7 @@ class IScreen {
 		virtual void load() = 0;	// when screen initialised
 		virtual void update() = 0;	// called periodically
 		virtual void exit() = 0;	// cleaning up
+									//
+		// returns a pointer to content area container if this screen supports nesting
+		virtual UIWidget* getContentContainer() { return nullptr; }
 };
