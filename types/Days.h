7aabf65107eb45b2186c33b39821b3e08c8bab77
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-15 11:31:06 +0100

    Turn Days into a class with a struct value (mask)

--- types/Days.h
+++ types/Days.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <cstdint>
 
-enum class Days : uint8_t {
-    Sunday    = 1 << 0, // 00000001
-    Monday    = 1 << 1, // 00000010
-    Tuesday   = 1 << 2, // 00000100
-    Wednesday = 1 << 3, // 00001000
-    Thursday  = 1 << 4, // 00010000
-    Friday    = 1 << 5, // 00100000
-    Saturday  = 1 << 6  // 01000000
+// wrapper struct wraps day mask
+struct Days {
+	enum Mask : uint8_t {
+		Sunday    = 1 << 0, // 00000001
+		Monday    = 1 << 1, // 00000010
+		Tuesday   = 1 << 2, // 00000100
+		Wednesday = 1 << 3, // 00001000
+		Thursday  = 1 << 4, // 00010000
+		Friday    = 1 << 5, // 00100000
+		Saturday  = 1 << 6  // 01000000
+	};
+
+	Mask value;
+
+	Days(Mask v) : value(v) {}
+
+	int daysUntilNextActive(int currentDay) const;
+
+	Days operator|(const Days rhs) {
+		return static_cast<Mask>(value | rhs.value);
+	}
+
+	bool operator&(const Days rhs) {
+		return value & rhs.value;
+	}
 };
-
-inline Days operator|(const Days lhs, const Days rhs) {
-	return static_cast<Days>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs));
-}
-
-inline bool operator&(const Days lhs, const Days rhs) {
-	return static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs);
-}
