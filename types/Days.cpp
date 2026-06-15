683e8735e2071ccee47e0b480c731464ac432528
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-15 11:32:11 +0100

    Implement daysUntilNextActive method on Days class
    Loops through days in the mask checking whether they are active, returns
    offset of first active one with respect to the current day. Note: the
    first active day may be the current one!

--- types/Days.cpp
+++ types/Days.cpp
@@ -1,2 +1,18 @@
 #include "Days.h"
 
+int Days::daysUntilNextActive(int currentDay) const {
+	// current day is just index e.g. 0 = Sunday, 1 = Monday, ...
+	
+	// loop through, checking each day starting from today
+	for (int i = 0; i < 7; i++) {
+		int nextDayIndex = (currentDay + i) % 7;
+
+		Mask nextDayMask = static_cast<Mask>(1 << nextDayIndex);
+
+		if (this->value & nextDayMask) {
+			return i;
+		}
+	}
+	// not found
+	return -1;
+}
