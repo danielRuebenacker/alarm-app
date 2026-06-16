5113791c7eacde937340e22a94f8c79a024d6a1e
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-16 17:02:08 +0100

    Change basic constructor tests to use 0:30 and invalid argument tests to check hour = 24 and min = 60 to better cover edge cases

--- tests/types/TimePointTest.cpp
+++ tests/types/TimePointTest.cpp
@@ -5,10 +5,10 @@
 #include "types/Days.h"
 
 TEST_CASE("Test basic constructor and hour, min, day methods") {
-	SUBCASE("Test timepoint for: Sunday 14:30") { 
-		TimePoint tp(14, 30, Days::Sunday);
+	SUBCASE("Test timepoint for: Sunday 0:30") { 
+		TimePoint tp(0, 30, Days::Sunday);
 
-		CHECK(tp.hour() == 14);
+		CHECK(tp.hour() == 0);
 		CHECK(tp.min() == 30);
 		CHECK(tp.day() == Days::Sunday);
 	}
@@ -27,11 +27,11 @@ TEST_CASE("Test constructor argument validation") {
 	}
 
 	SUBCASE("Check invalid min throws invalid_argument exception") {
-		CHECK_THROWS_AS(TimePoint tp(9, 65, Days::Sunday), std::invalid_argument);
+		CHECK_THROWS_AS(TimePoint tp(9, 60, Days::Sunday), std::invalid_argument);
 	}
 
 	SUBCASE("Check invalid hour and min throw invalid_argument exception") {
-		CHECK_THROWS_AS(TimePoint tp(24, 65, Days::Sunday), std::invalid_argument);
+		CHECK_THROWS_AS(TimePoint tp(24, 60, Days::Sunday), std::invalid_argument);
 	}
 
 	SUBCASE("Check invalid_argument exception thrown on negative numbers") {
