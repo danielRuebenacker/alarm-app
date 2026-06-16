6bf7bbfeca259313fa8afde0f2c3a83cc9a650e2
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-16 16:54:13 +0100

    Add tests for TimePoint: constructor and validation of hour and minute.

---
+++ tests/types/TimePointTest.cpp
@@ -0,0 +1,38 @@
+#include "../doctest.h"
+#include <stdexcept>
+
+#include "types/TimePoint.h"
+#include "types/Days.h"
+
+TEST_CASE("Test basic constructor and hour, min, day methods") {
+	SUBCASE("Test timepoint for: Sunday 14:30") { 
+		TimePoint tp(14, 30, Days::Sunday);
+
+		CHECK(tp.hour() == 14);
+		CHECK(tp.min() == 30);
+		CHECK(tp.day() == Days::Sunday);
+	}
+
+	SUBCASE("Test timepoint for: Monday, 9:55") { 
+		TimePoint tp(9, 55, Days::Monday);
+
+		CHECK(tp.hour() == 9);
+		CHECK(tp.min() == 55);
+		CHECK(tp.day() == Days::Monday);
+	}
+}
+TEST_CASE("Test constructor argument validation") {
+	SUBCASE("Check invalid hour throws invalid_argument exception") {
+		CHECK_THROWS_AS(TimePoint tp(24, 55, Days::Sunday), std::invalid_argument);
+	}
+
+	SUBCASE("Check invalid min throws invalid_argument exception") {
+		CHECK_THROWS_AS(TimePoint tp(9, 65, Days::Sunday), std::invalid_argument);
+	}
+}
+
+// TEST_CASE("Test minutesSinceMidnight method") {
+// 	SUBCASE("Check minutes since midnight for midnight time is 0") {
+//
+// 	}
+// }
