f75336f8f0a1771771c08bdac4c96e704727ee65
danielRuebenacker <danielrubenacker2@gmail.com>
2026-06-16 15:58:54 +0100

    Test isActive and hasAnyDays on no days and single days and test daysUntilNextActive

--- tests/types/DaysTest.cpp
+++ tests/types/DaysTest.cpp
@@ -2,9 +2,30 @@
 
 #include "types/Days.h"
 
-TEST_CASE("Simple check setting days works") {
+TEST_CASE("Test days bitmask (basic)") {
 	Days days;
-	days.set(Days::Friday);
-	CHECK(days.isActive(Days::Friday) == 1);
+
+	SUBCASE("Check isActive and hasAnyDays return false when no days set") {
+		CHECK(days.isActive(Days::Friday) == false);
+		CHECK(days.hasAnyDays() == false);
+	}
+
+	SUBCASE("Check adding single day, isActive and hasAnyDays should return true") {
+		days.set(Days::Friday);
+		CHECK(days.hasAnyDays() == true);
+		CHECK(days.isActive(Days::Friday) == true);
+	}
 }
 
+TEST_CASE("Test daysUntilNextActive function") {
+	Days days;
+
+	SUBCASE("Test empty mask returns -1") {
+		CHECK(days.daysUntilNextActive(Days::Monday) == -1);
+	}
+
+	SUBCASE("Check how many days to next active day (today Sunday, Monday active)") {
+		days.set(Days::Monday);
+		CHECK(days.daysUntilNextActive(Days::Sunday) == 1);
+	}
+}
