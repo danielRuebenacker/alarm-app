2e7752d6c7d50f7bbced2026c72e7ec7e1c7f615
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-01 16:53:11 +0100

    Add MathPuzzle abstract class.
    Extracts int answer from PuzzleResponse and checks against protected
    variable correctAnswer. Subclasses to come.

--- src/puzzles/MathPuzzle.h
+++ src/puzzles/MathPuzzle.h
@@ -1,7 +1,31 @@
 #pragma once
 #include "../interfaces/IPuzzle.h"
+#include "../types/Difficulty.h"
+#include "RandomNumberGenerator.h"
+#include <cmath>
 
 class MathPuzzle : public IPuzzle {
+    public:
+        virtual std::string toString() const = 0;
+
+        bool verifySolution(const PuzzleResponse &userAnswer) override {
+            // get const pointer to value
+            const int* val = std::get_if<int>(&userAnswer.value);
+            // dereference
+            if (*val == correctAnswer) {
+                return true;
+            }
+            return false;
+        }
+    protected:
+        int correctAnswer;
+};
+
+class EasyMathPuzzle : public MathPuzzle {
+	private:
+		int x, y, z;
+		RandomNumberGenerator& rd_;
+
 	public:
 		void display(UIWidget* wrapper) override {}
 
