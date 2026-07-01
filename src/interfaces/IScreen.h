adc673506537fdf0723890675a1f5d58a12a3dc0
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-01 16:50:40 +0100

    Remove getContentContainer from IScreen
    Need to rethink design but this was always a code smell.

--- src/interfaces/IScreen.h
+++ src/interfaces/IScreen.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "IUIWidget.h"
 
 class IScreen {
 	public:
@@ -7,7 +6,4 @@ class IScreen {
 		virtual void load() = 0;	// when screen initialised
 		virtual void update() = 0;	// called periodically
 		virtual void exit() = 0;	// cleaning up
-									//
-		// returns a pointer to content area container if this screen supports nesting
-		virtual UIWidget* getContentContainer() { return nullptr; }
 };
