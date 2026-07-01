138f9c22eab770d43020a3f585ea0b4626174c45
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-01 16:49:06 +0100

    Add RandomNumberGenerator to PuzzleFactory and make non-static
    Since math puzzles now require random number generators.

--- src/domain/PuzzleFactory.h
+++ src/domain/PuzzleFactory.h
@@ -1,17 +1,21 @@
 #pragma once
 #include "../interfaces/IPuzzle.h"
 #include "../types/PuzzleType.h"
-#include "../puzzles/MathPuzzle.cpp"
+#include "../puzzles/MathPuzzle.h"
 
 #include <memory>
 #include <stdexcept>
 
 class PuzzleFactory {
+    RandomNumberGenerator& rd_;
+
 	public:
-		static std::unique_ptr<IPuzzle> createPuzzle(PuzzleType type) {
+        PuzzleFactory(RandomNumberGenerator& rd) : rd_(rd) {}
+
+		std::unique_ptr<IPuzzle> createPuzzle(PuzzleType type) {
 			switch(type) {
 				case PuzzleType::MATHS:
-					return std::make_unique<MathPuzzle>();
+					return std::make_unique<EasyMathPuzzle>(rd_);
 				case PuzzleType::CAPTCHA:
 					return nullptr;
 				default:
