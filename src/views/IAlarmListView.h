bd7e5d00a9a2cc5be9795b168783856437b8c382
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-02 16:36:50 +0100

    Add IAlarmListView interface
    Includes function to display all alarms (a list), and methods to
    specific what should be done when alarms are toggled, the back button is
    pressed (should go home) and what happens when a specific alarm is
    pressed (should go to alarm config page).

---
+++ src/views/IAlarmListView.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include "../domain/Alarm.h"
+
+class IAlarmListView {
+    public:
+        virtual ~IAlarmListView() = default;
+        // list all alarms (with info and toggles)
+        virtual void displayListOfAlarms(const std::vector<Alarm> listOfAlarms) = 0;
+        // toggle specific alarm
+        virtual void setOnAlarmToggled(std::function<void(int)> callback) = 0;
+        // button to take home
+        virtual void setOnBackButtonPress(std::function<void()> callback) = 0;
+        // when alarm pressed, take to its config (using int alarmID)
+        virtual void setOnAlarmPressed(std::function<void(int)> callback) = 0;
+};
