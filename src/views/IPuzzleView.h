bab9166cec5d3e6b9fa66bcdcc73179ff8eee502
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-02 16:46:21 +0100

    Add IPuzzleView interface
    Specify there is a timeout bar that is updated (shows how much time left
    before the puzzle times out and the alarm is rerung). Also specify how
    puzzle is loaded and ability for presenter to speficy behaviour when
    submit button is pressed (puzzle then validated).

---
+++ src/views/IPuzzleView.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <functional>
+
+#include "../interfaces/IPuzzle.h"
+#include "../types/PuzzleType.h"
+#include "../types/PuzzleResponse.h"
+
+class IPuzzleView {
+    public:
+        virtual ~IPuzzleView() = default;
+        virtual void updateTimeoutBar(int secondsLeft) = 0;
+        virtual void loadPuzzle(const PuzzleType& puzzleType, const IPuzzle& puzzle) = 0;
+        virtual void setOnSubmitCallback(std::function<void(const PuzzleResponse& response)> callback) = 0;
+};
