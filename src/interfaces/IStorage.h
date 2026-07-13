441bfb248f7fd58679a5c5b2cce02d4a5cc82077
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-13 01:29:55 +0100

    Add method for saving/loading dismissed alarms to IStorage

--- src/interfaces/IStorage.h
+++ src/interfaces/IStorage.h
@@ -7,5 +7,7 @@ class IStorage {
 	public: 
 		virtual ~IStorage() = default;
 		virtual void saveAlarms(const std::vector<Alarm> alarms) = 0;
+		virtual void saveDismissed(const std::vector<int> dismissedAlarmIds) = 0;
 		virtual std::vector<Alarm> loadAlarms() const = 0;
+        virtual std::vector<int> loadDismissedAlarmIds() = 0;
 };
