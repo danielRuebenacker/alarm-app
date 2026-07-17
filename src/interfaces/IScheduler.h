2efd68904fc1d49ed4750f63bbc7da750e38c6af
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-17 13:28:21 +0100

    Add IScheduler interface
    Responsible for literal timer setting, and executing the given
    callback on timer timeout. Will add an AlarmScheduler class which uses
    a concrete IScheduler to actually set alarms with callbacks.

---
+++ src/interfaces/IScheduler.h
@@ -0,0 +1,16 @@
+#include <cstdint>
+#include <functional>
+#include <chrono>
+
+// abstract class for platform specific scheduler
+class IScheduler {
+	using TimerHandle = std::uintptr_t;
+	static constexpr TimerHandle kInvalidHandle = 0;
+
+	virtual ~IScheduler() = default;
+
+	virtual TimerHandle scheduleOnce(std::chrono::milliseconds delay,
+										std::function<void()> callback) = 0;
+
+	virtual void cancel(TimerHandle handle) = 0;
+};
