47880c749ec2a1e21e23d7b8fbd1743814b6f3e5
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-23 17:45:06 +0100

    Chnage list of alarms to references and add setOnAddAlarmClicked method

--- src/UI/views/IAlarmListView.h
+++ src/UI/views/IAlarmListView.h
@@ -9,11 +9,13 @@ class IAlarmListView : public View {
     public:
         virtual ~IAlarmListView() = default;
         // list all alarms (with info and toggles)
-        virtual void displayListOfAlarms(const std::vector<Alarm> listOfAlarms) = 0;
+        virtual void displayListOfAlarms(const std::vector<Alarm>& listOfAlarms) = 0;
         // toggle specific alarm
         virtual void setOnAlarmToggled(std::function<void(int)> callback) = 0;
         // button to take home
         virtual void setOnBackButtonPress(std::function<void()> callback) = 0;
         // when alarm pressed, take to its config (using int alarmID)
         virtual void setOnAlarmPressed(std::function<void(int)> callback) = 0;
+		// create alarms (leads to empty config)
+		virtual void setOnAddAlarmClicked(std::function<void> callback) = 0;
 };
