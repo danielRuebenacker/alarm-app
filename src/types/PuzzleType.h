bac26e09aa0d59bf7e2646da63d0df6155557f46
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-23 17:38:46 +0100

    Add toString to PuzzleType using <string_view>

--- src/types/PuzzleType.h
+++ src/types/PuzzleType.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <string_view>
 
 enum PuzzleType {
 	MATHS,
 	MEMORY,
 	CAPTCHA
 };
+
+constexpr std::string_view to_string(PuzzleType type) {
+    switch (type) {
+        case PuzzleType::MATHS:   return "MATHS";
+        case PuzzleType::MEMORY:  return "MEMORY";
+        case PuzzleType::CAPTCHA: return "CAPTCHA";
+        default:                  return "UNKNOWN";
+    }
+}
