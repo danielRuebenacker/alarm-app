099e245b4c1a8727fe4bd11cfd7c0d3aa54182d6
danielRuebenacker <danielrubenacker2@gmail.com>
2026-07-24 12:05:25 +0100

    Delete AppStates (moving away from states model).

--- src/appStates/IdleState.h
+++
@@ -1,8 +0,0 @@
-#include "../interfaces/IAppState.h"
-
-class IdleState : public IAppState {
-	public:
-		void update(AlarmContext* context) override {
-			// regular, don't need to check for alarms
-		}
-};
